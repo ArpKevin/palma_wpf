697b26acb09adafdd7422c7645e8811473d8642d
C20ÁrpásK <C20rpsK@204-09>
2024-09-20 12:16:43 +0200

    Add project files.

---
+++ Pálma/Dessert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pálma
+{
+    public class Dessert {
+        public string DessertName { get; set; }
+        public string DessertType { get; set; }
+        public bool HasDessertBeenAwarded { get; set; }
+        public int DessertPrice { get; set; }
+        public string DessertUnit { get; set; }
+
+        public Dessert(string dessertName, string dessertType, bool hasDessertBeenAwarded, int dessertPrice, string dessertUnit)
+        {
+            DessertName = dessertName;
+            DessertType = dessertType;
+            HasDessertBeenAwarded = hasDessertBeenAwarded;
+            DessertPrice = dessertPrice;
+            DessertUnit = dessertUnit;
+        }
+
+        public override string ToString()
+        {
+            //return $"Desszert neve: {DessertName}";
+        }
+    }   
+}
---
+++ Pálma/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pálma
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var desserts = new List<Dessert>();
+
+            string palmaSrc = @"..\..\..\src\palma.txt";
+
+            foreach (var item in File.ReadAllLines(palmaSrc))
+            {
+                var x = palmaSrc.Split(";");
+                desserts.Add(new(x[0], x[1], bool.Parse(x[2]), int.Parse(x[3]), x[4]));
+            }
+        }
+    }
+}
